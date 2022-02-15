297f37799b17b0535dac578a0f42e54ee5e6ffb3
20127348 <98143233+20127348@users.noreply.github.com>
2022-02-15 10:42:52 +0700

    Add files via upload

---
+++ SnakeGame/gotoxy.h
@@ -0,0 +1,13 @@
+#ifndef __gotoxy_H__
+#define __gotoxy_H__
+#include<Windows.h>
+void gotoxy(int x, int y)
+{
+	static HANDLE h = NULL;
+	if (!h)
+		h = GetStdHandle(STD_OUTPUT_HANDLE);
+	COORD c = { x, y };
+	SetConsoleCursorPosition(h, c);
+}
+
+#endif
