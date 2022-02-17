131ce4a5c7ada66ec8aecf15a76a49a71c577126
20127348 <98143233+20127348@users.noreply.github.com>
2022-02-17 15:26:21 +0700

    Create OneDimensionArray.cpp

--- HomeworkWeek3/20127142_20127348_19127652_20127652/OneDimensionArray.cpp
+++ HomeworkWeek3/20127142_20127348_19127652_20127652/OneDimensionArray.cpp
@@ -1,11 +1,12 @@
 void InputArray_1D(int*& a, int& n)
 {
-
+  a = new int[n];
 }
 
 void FreeArray(int*& a)
 {
-	delete[]a;
+for (int i = 0; &a[i] != nullptr; i++)
+		delete[]a;
 }
 
 void OutputArray_1D(int* a, int n)
