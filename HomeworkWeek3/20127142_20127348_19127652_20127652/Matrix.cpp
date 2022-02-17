6703fd3030c7e079a88146518c208019fd7205d2
Minh-Triet-sys <81966370+Minh-Triet-sys@users.noreply.github.com>
2022-02-17 15:39:57 +0700

    Update Matrix.cpp

--- HomeworkWeek3/20127142_20127348_19127652_20127652/Matrix.cpp
+++ HomeworkWeek3/20127142_20127348_19127652_20127652/Matrix.cpp
@@ -5,7 +5,12 @@ int** CreateMatrix(int n)
 
 void PrintMatrix(int** matrix, int n)
 {
-	
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			cout << matrix[i][j] << " ";
+		}
+		cout << endl;
+	}
 }
 
 void FreeMatrix(int** matrix, int n)
