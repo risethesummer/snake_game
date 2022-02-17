951eb9a526a159eb7efffafbc8ddea3357da61dc
risethesummer <honhatlinhmail@gmail.com>
2022-02-17 15:06:25 +0700

    week 3

---
+++ HomeworkWeek3/20127142_20127348_19127652_20127652/Matrix.cpp
@@ -0,0 +1,14 @@
+int** CreateMatrix(int n)
+{
+
+}
+
+void PrintMatrix(int** matrix, int n)
+{
+	
+}
+
+void FreeMatrix(int** matrix, int n)
+{
+
+}
---
+++ HomeworkWeek3/20127142_20127348_19127652_20127652/OneDimensionArray.cpp
@@ -0,0 +1,14 @@
+void InputArray_1D(int*& a, int& n)
+{
+
+}
+
+void FreeArray(int*& a)
+{
+
+}
+
+void OutputArray_1D(int* a, int n)
+{
+
+}
