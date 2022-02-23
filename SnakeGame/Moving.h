7220676b6d84cfeb74ec062d8f990eafc6e48819
Minh-Triet-sys <81966370+Minh-Triet-sys@users.noreply.github.com>
2022-02-23 08:57:48 +0700

    Add files via upload

---
+++ SnakeGame/Moving.h
@@ -0,0 +1,14 @@
+#ifndef _MOVING_H_
+#define _MOVING_H_
+#define WIDTH_CONSOLE 20
+#define HEIGH_CONSOLE 50
+int SIZE_SNAKE = 6;
+#include <iostream>
+using namespace std;
+
+void MoveRight();
+void MoveLeft();
+void MoveDown();
+void MoveUp();
+
+#endif
