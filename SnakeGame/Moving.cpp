76b3775e7416e08e3e3869498b02ae490b116d17
Minh-Triet-sys <81966370+Minh-Triet-sys@users.noreply.github.com>
2022-02-23 08:56:59 +0700

    Add files via upload

---
+++ SnakeGame/Moving.cpp
@@ -0,0 +1,69 @@
+void MoveRight() {
+		if (snake[SIZE_SNAKE - 1].x + 1 == WIDTH_CONSOLE) {
+				ProcessDead();
+		}
+	
+		else {
+				if (snake[SIZE_SNAKE - 1].x + 1 == food[FOOD_INDEX].x && snake[SIZE_SNAKE - 1].y == food[FOOD_INDEX].y) {
+						Eat();
+				}
+				for (int i = 0; i < SIZE_SNAKE - 1; i++) {
+					snake[i].x = snake[i + 1].x;
+					snake[i].y = snake[i + 1].y;
+				}
+			 snake[SIZE_SNAKE - 1].x++;
+		}
+}
+
+ void MoveLeft() {
+	 if (snake[SIZE_SNAKE - 1].x - 1 == 0) {
+		 ProcessDead();
+	 }
+	 else {
+		  if (snake[SIZE_SNAKE - 1].x - 1 == food[FOOD_INDEX].x && snake[SIZE_SNAKE - 1].y == food[FOOD_INDEX].y) 
+		  {
+			  Eat();
+		  }
+		  for (int i = 0; i < SIZE_SNAKE - 1; i++) {
+			  snake[i].x = snake[i + 1].x;
+			  snake[i].y = snake[i + 1].y;
+		 }
+		  snake[SIZE_SNAKE - 1].x--;
+	 }
+ }
+ 
+ void MoveDown() {
+	 if (snake[SIZE_SNAKE - 1].y + 1 == HEIGH_CONSOLE) {
+		 ProcessDead();
+	 }
+	 else {
+		 if (snake[SIZE_SNAKE - 1].x == food[FOOD_INDEX].x && snake[SIZE_SNAKE - 1].y + 1 == food[FOOD_INDEX].y)
+		 {
+			 Eat();
+		 }
+		 for (int i = 0; i < SIZE_SNAKE - 1; i++) {
+			 snake[i].x = snake[i + 1].x;
+			 snake[i].y = snake[i + 1].y;
+		 }
+		 snake[SIZE_SNAKE - 1].y++;
+	 }
+ }
+
+ void MoveUp() {
+		  if (snake[SIZE_SNAKE - 1].y - 1 == 0) {
+			  ProcessDead();
+		  }
+		  else {
+			  if (snake[SIZE_SNAKE - 1].x == food[FOOD_INDEX].x && snake[SIZE_SNAKE - 1].y - 1 == food[FOOD_INDEX].y)
+			  {
+				  Eat();
+			  }
+			  for (int i = 0; i < SIZE_SNAKE - 1; i++) {
+				  snake[i].x = snake[i + 1].x;
+				  snake[i].y = snake[i + 1].y;
+			  }
+			  snake[SIZE_SNAKE - 1].y--;
+		  }
+ }
+	 
+	
