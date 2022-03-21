a5227fc0f1b6a7b03251c7ca9160fb64d8991adf
DƯƠNG LÊ PHI <20127142@student.hcmus.edu.vn>
2022-03-21 13:07:17 +0700

    complete processDead

--- SnakeGame/processDead.cpp
+++ SnakeGame/processDead.cpp
@@ -1,39 +1,13 @@
 #include "ProcessDead.h"
-//void ProcessDead() {
-//	STATE = 0;
-//	score = 0;
-//	LIFE--;
-//	if (LIFE == 0) {
-//		PlaySound(TEXT("game_over.wav"), NULL, SND_ASYNC);
-//		GotoXY(0, HEIGH_CONSOLE + 2);
-//		printf("GAME OVER!!!!!!!!!!!");
-//		cout << endl;
-//		cout << "PRESS Y TO RESTART THE GAME, PRESS OTHER KEYS TO EXIT";
-//	}
-//	else 
-//		StartGame();
-//}
-//
-//void StartGame() {
-//	system("cls");
-//	ResetData(); // Khởi tạo dữ liệu gốc
-//	DrawBoard(0, 0, WIDTH_CONSOLE, HEIGH_CONSOLE);
-//	display();
-//	STATE = 1;
-//}
-//
-//void ResetData() {
-//	//Defaut values
-//	CHAR_LOCK = 'A', MOVING = 'D', SPEED = 1; FOOD_INDEX = 1, WIDTH_CONSOLE = 70,
-//		HEIGH_CONSOLE = 20, SIZE_SNAKE = 1; COUNT = 0;
-//
-//	//Default snake values
-//	snake[0] = { 10, 5 };
-//	GenerateFood();//ham food cua Thuan
-//}
 
 
 bool ProcessDead(const vector<Point> snake, const Point& topLeftAnchor, const Point& bottomRightAnchor)
 {
+	if ((snake[0].x > topLeftAnchor.x && snake[0].x < bottomRightAnchor.x) && (snake[0].y > topLeftAnchor.y && snake[0].y < bottomRightAnchor.y))
+		return true;
+	for (int i = 1; i < snake.size(); i++) {
+		if (snake[0].x == snake[i].x && snake[0].y == snake[i].y)
+			return true;
+	}
 	return false;
 }
