04858f24c4f5bebf6b8e805118b187e29d3cbb95
20127348 <98143233+20127348@users.noreply.github.com>
2022-04-08 13:18:42 +0700

    Update Gate.cpp

--- SnakeGame/Gate.cpp
+++ SnakeGame/Gate.cpp
@@ -1,5 +1,23 @@
 #include "Gate.h"
 
+bool checkGate(const Snake& snake, const Point& gate)
+{
+  for (Node* current = snake.head; current; current = current->next) {
+		if (current->position == gate)
+			return true;
+	}
+	return false;
+}
+
 Point createAndDrawGate(const Snake& snake, int topLeft, int bottomRight)
 {
+	srand(time(NULL));
+	Point gate;
+	do {
+			
+		gate.x = rand() % (bottomRight.x - topLeft.x) + topLeft.x;
+		gate.y = rand() % (bottomRight.y - topLeft.y) + topLeft.y;
+	} while (checkGate(snake, gate));
+	//print(gate, '▄', RED_RED);
+	return gate;
 }
